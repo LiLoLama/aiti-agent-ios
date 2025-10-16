40cfc5023c7dcbd036c068b1f07c894ca6b3ca6c
Liam Schmid <mail@liamschmid.com>
2025-10-16 14:39:18 +0200

    Delete src directory

--- src/hooks/useToggle.ts
+++
@@ -1,8 +0,0 @@
-import { useState, useCallback } from 'react';
-
-export function useToggle(initial = false): [boolean, () => void, (value: boolean) => void] {
-  const [value, setValue] = useState(initial);
-  const toggle = useCallback(() => setValue((prev) => !prev), []);
-  const set = useCallback((next: boolean) => setValue(next), []);
-  return [value, toggle, set];
-}
--- src/types/custom-events.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-  interface WindowEventMap {
-    'aiti-settings-update': CustomEvent<import('./settings').AgentSettingsEventPayload>;
-  }
-}
-
-export {};
