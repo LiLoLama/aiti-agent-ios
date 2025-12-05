f4a421f4cf957c80cd8a7abf172c57087b97cf65
Liam Schmid <mail@liamschmid.com>
2025-12-05 18:02:39 +0100

    Use namespaced AVAudioApplication record permission cases

--- ios/AITIExplorer/ViewModels/AudioViewModel.swift
+++ ios/AITIExplorer/ViewModels/AudioViewModel.swift
@@ -417,11 +417,11 @@ final class AudioViewModel: NSObject, ObservableObject {
     private func currentRecordPermission() -> AVAudioSession.RecordPermission {
         if #available(iOS 17, *) {
             switch AVAudioApplication.shared.recordPermission {
-            case .undetermined:
+            case AVAudioApplication.recordPermission.undetermined:
                 return .undetermined
-            case .denied:
+            case AVAudioApplication.recordPermission.denied:
                 return .denied
-            case .granted:
+            case AVAudioApplication.recordPermission.granted:
                 return .granted
             @unknown default:
                 return .undetermined
